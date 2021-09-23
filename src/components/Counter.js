54530c5ebf23980c4ec68131daa75d67671f6334
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-09-23 07:54:40 +0530

    +useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,24 @@
 import classes from './Counter.module.css';
 
+//!the use of this useSelector is : 
+//! 1. Fetching the a piece of state from store as in redux the store has alll the states
+//! in one place but we dont need the whole data
+//! 2. It automatically assigns a SUBSCRIBER function to the component for the latest state 
+//!and when component is umnounted from dom the subscriber function is also removed from it
+
+import { useSelector } from 'react-redux';
 const Counter = () => {
+
+  //!state => state.counter : means we need only the "counter" part of state from the store
+  //!as of now only counter state is there but if there were a lots of states then it makes more sense
+  const counter = useSelector(state => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//! importing provider whose function is same as of a context provider
+import {Provider} from 'react-redux'
+
+//!importing the store thatwe created and it will have all the data stored 
+import store from './store';
+
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//!to let the redux provider know what it has to provide or from where it has prop named store
+//!we pass the store we created to this Provider andconnect them
+ReactDOM.render(<Provider store={store}><App /></Provider> ,document.getElementById('root'));
